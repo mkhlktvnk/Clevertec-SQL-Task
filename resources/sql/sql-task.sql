dfe1d700db48e7316b276cbc95331aac34f5cc9b
misha <mikhail.kutovenko@gmail.com>
2023-03-04 14:12:50 +0300

    Update sql-task.sql: add task3

--- resources/sql/sql-task.sql
+++ resources/sql/sql-task.sql
@@ -13,3 +13,13 @@ FROM seats
          INNER JOIN aircrafts ON aircrafts.aircraft_code = seats.aircraft_code
 GROUP BY aircrafts.model
 ORDER BY seat_count DESC LIMIT 3;
+
+/* TASK-3 */
+
+SELECT aircrafts.aircraft_code, aircrafts.model, seats.seat_no
+FROM aircrafts
+         JOIN seats on aircrafts.aircraft_code = seats.aircraft_code
+WHERE aircrafts.model LIKE 'Аэробус A321-200'
+  AND seats.fare_conditions NOT LIKE 'Economy'
+GROUP BY aircrafts.aircraft_code, aircrafts.model, seats.seat_no
+ORDER BY seats.seat_no;
