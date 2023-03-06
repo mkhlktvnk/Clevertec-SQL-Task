32185fcd2ec82e9de6cad32e2cfa71138949ef54
misha <mikhail.kutovenko@gmail.com>
2023-03-06 18:01:55 +0300

    Update sql-task.sql: refactor

--- resources/sql/sql-task.sql
+++ resources/sql/sql-task.sql
@@ -1,26 +1,31 @@
 /* TASK-1 */
 
-SELECT aircrafts.aircraft_code, aircrafts.model, seats.fare_conditions, count(seats.seat_no)
-FROM aircrafts
-         JOIN seats ON aircrafts.aircraft_code = seats.aircraft_code
-GROUP BY aircrafts.aircraft_code, aircrafts.model, seats.fare_conditions
-ORDER BY aircraft_code;
+SELECT aircrafts_data.aircraft_code        as code,
+       aircrafts_data.model::json ->> 'ru' as model,
+       seats.fare_conditions               as fare_conditions,
+       count(seats.seat_no)                as seat_count
+FROM aircrafts_data
+         JOIN seats ON aircrafts_data.aircraft_code = seats.aircraft_code
+GROUP BY aircrafts_data.aircraft_code, aircrafts_data.model, seats.fare_conditions
+ORDER BY aircrafts_data.aircraft_code;
 
 /* TASK-2 */
 
-SELECT aircrafts.model, count(seats.seat_no) AS seat_count
+SELECT aircrafts_data.model::json ->> 'ru' AS model,
+       count(seats.seat_no)                AS seat_count
 FROM seats
-         JOIN aircrafts ON aircrafts.aircraft_code = seats.aircraft_code
-GROUP BY aircrafts.model
+         JOIN aircrafts_data ON aircrafts_data.aircraft_code = seats.aircraft_code
+GROUP BY aircrafts_data.model
 ORDER BY seat_count DESC
 LIMIT 3;
 
 /* TASK-3 */
 
-SELECT aircrafts.aircraft_code, aircrafts.model, seats.seat_no
-FROM aircrafts
-         JOIN seats on aircrafts.aircraft_code = seats.aircraft_code
-WHERE aircrafts.model LIKE 'Аэробус A321-200'
+SELECT aircrafts_data.aircraft_code        AS code,
+       aircrafts_data.model::json ->> 'ru' AS model,
+       seats.seat_no                       AS seats
+FROM aircrafts_data
+         JOIN seats ON aircrafts_data.aircraft_code = seats.aircraft_code
+WHERE aircrafts_data.model::json ->> 'ru' LIKE 'Аэробус A321-200'
   AND seats.fare_conditions NOT LIKE 'Economy'
-GROUP BY aircrafts.aircraft_code, aircrafts.model, seats.seat_no
-ORDER BY seats.seat_no;
+ORDER BY seats;
